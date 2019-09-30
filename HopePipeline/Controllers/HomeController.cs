cb5c769f015220c507e628942ca8b8094e2b540a
Brandi Lauria <n01057930@unf.edu>
2019-09-30 13:34:05 -0400

    I made the referall form and associated controllers

--- HopePipeline/Controllers/HomeController.cs
+++ HopePipeline/Controllers/HomeController.cs
@@ -31,9 +31,101 @@ namespace HopePipeline.Controllers
 
         public IActionResult Referral()
         {
+            int here = 0;
+
+
+            int maxorder = 0;
+            maxorder = db.Referrals.Count();
+            if (maxorder == 0)
+            {
+                here = 1;
+
+            }
+            else { here = (db.Referrals.OrderBy(p => p.Id).Max(p => p.Id) + 1); }
+
+
+            ViewBag.ms = DateTime.Today;
+            ViewBag.PKvalue = here;
+
+            ViewBag.Status = "Pending";
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(string Mother, string Father, string Guardian, string Familymember, string FosterParent, string VariableFM, string Failed,
+             string Suspended, string Alt, string Plan, string Notenrolled, string Hxbaker, string Meeting, string meetingtime,
+             string School, string Reach,
+             [Bind(Include = "Id,First_Name,Last_Name,Date_of_Birth,Primary_Caretaker_s_Name,Primary_Caretaker_s_Relationship" +
+                ",Street_Address,Parents_Guardian_s_Email_Address,Parents_Guardian_s_Phone_number,Upcoming_meeting_scheduled_with_school," +
+                "date,Is_the_youth_a_student_in_a_Duval_County_Public_School,Issues_Impacting_student,Where_is_the_youth_currently_attending_school," +
+                "Is_there_anything_else_you_would_like_us_to_know_about_the_youth_or_their_family" +
+                ",What_is_the_best_way_to_communicate_with_the_youth_family,zip_code,Status," +
+                "Date_Referral_Input,email_address,Time")] Referral referral)
 
+
+
+        {
+            if (ModelState.IsValid)
+            {
+
+
+
+                //First Checkbox
+                //answer equals checkboxes of guardians for client
+                string answer = null;
+                if (Mother != null)
+                { answer = Mother + "," + ""; }
+                if (Father != null)
+                { answer = answer + Father + "," + ""; }
+                if (Guardian != null)
+                { answer = answer + Guardian + "," + ""; }
+                if (Familymember != null)
+                { answer = answer + Familymember + "," + ""; }
+                if (FosterParent != null)
+                { answer = answer + FosterParent + "," + ""; }
+                if (VariableFM != null)
+                { answer = answer + VariableFM + "," + ""; }
+                referral.Primary_Caretaker_s_Relationship = answer;
+
+
+                //Second Checkbox
+                string answer1 = null;
+                if (Failed != null)
+                { answer1 = Failed + "," + ""; }
+                if (Suspended != null)
+                { answer1 = answer1 + Suspended + "," + ""; }
+                if (Alt != null)
+                { answer1 = answer1 + Alt + "," + ""; }
+                if (Plan != null)
+                { answer1 = answer1 + Plan + "," + ""; }
+                if (Notenrolled != null)
+                { answer1 = answer1 + Notenrolled + "," + ""; }
+                if (Hxbaker != null)
+                { answer1 = answer1 + Hxbaker + "," + ""; }
+                referral.Issues_Impacting_student = answer1;
+
+
+                referral.Is_the_youth_a_student_in_a_Duval_County_Public_School = School;
+
+                referral.Upcoming_meeting_scheduled_with_school = Meeting;
+
+                referral.Time = meetingtime;
+
+
+
+
+                referral.What_is_the_best_way_to_communicate_with_the_youth_family = Reach;
+
+
+                db.Configuration.ValidateOnSaveEnabled = false;
+                db.Referrals.Add(referral);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(referral);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ HopePipeline/Models/Referral.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HopePipeline.Models
+{
+    public class Referral
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        //done
+        public string First_Name { get; set; }
+        //done
+        public string Last_Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> Date_of_Birth { get; set; }
+        //done
+        public string Primary_Caretaker_s_Name { get; set; }
+        //done
+        public string Primary_Caretaker_s_Relationship { get; set; }
+        //done
+        public string Street_Address { get; set; }
+        //done
+        [Required(ErrorMessage = "Please Provide a Email Address", AllowEmptyStrings = false)]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")]
+        public string Parents_Guardian_s_Email_Address { get; set; }
+        //done
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "This a Required Field", AllowEmptyStrings = false)]
+        public string Parents_Guardian_s_Phone_number { get; set; }
+
+        public string Upcoming_meeting_scheduled_with_school { get; set; }
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> date { get; set; }
+
+        public string Is_the_youth_a_student_in_a_Duval_County_Public_School { get; set; }
+        public string Issues_Impacting_student { get; set; }
+        //done
+        public string Where_is_the_youth_currently_attending_school { get; set; }
+        //done
+        public string Is_there_anything_else_you_would_like_us_to_know_about_the_youth_or_their_family { get; set; }
+
+        public string What_is_the_best_way_to_communicate_with_the_youth_family { get; set; }
+
+        //done
+        public string zip_code { get; set; }
+
+        //auto input
+        public string Status { get; set; }
+        //done
+        public Nullable<System.DateTime> Date_Referral_Input { get; set; }
+
+        [Required(ErrorMessage = "Please Provide a Email Address", AllowEmptyStrings = false)]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")]
+        public string email_address { get; set; }
+        public string Time { get; set; }
+    }
+}
