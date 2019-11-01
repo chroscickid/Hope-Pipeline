c0075f83b0dd3eeab7e62dcb26646444699e80e6
Lili Weinstein <n00909733@ospreys.unf.edu>
2019-10-31 20:05:46 -0400

    Did the tracking form

--- HopePipeline/Models/Tracking.cs
+++ HopePipeline/Models/Tracking.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +7,41 @@ namespace HopePipeline.Models
 {
     public class Tracking
     {
+        public int ClientID { get; set; }
+        public string Name { get; set; }
+        public string caregender { get; set; }
+        public string carename { get; set; }
+        public string gender { get; set; }
+        public string ethnicity { get; set; }
+        public string dob { get; set; }
+        public string school { get; set; }
+        public string referral { get; set; }
+        public string fail { get; set; }
+        public string failedGrade { get; set; }
+        public string baker { get; set; }
+        public string femHouse { get; set; }
+        public string domVio { get; set; }
+        public string adopted { get; set; }
+        public string evicted { get; set; }
+        public string incarParent { get; set; }
+        public string asthma { get; set; }
+        public string iep { get; set; }
+        public string inPride { get; set; }
+        public string newFBA { get; set; }
+        public string accomGained { get; set; }
+        public string compService { get; set; }
+        public string services { get; set; }
+        public string bullied { get; set; }
+        public string report { get; set; }
+        public string dateofBully { get; set; }
+        public string suspended { get; set; }
+        public string altSchool { get; set; }
+        public string altSchoolName { get; set; }
+        public string dateOfAlt { get; set; }
+        public string timesInAlt { get; set; }
+        public string daysOwed { get; set; }
+        public string firstLegal { get; set; }
+        public string secondLegal { get; set; }
+        
     }
-}
+}*/
