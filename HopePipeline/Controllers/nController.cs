9f4e13db883b44427a41e99d62ee900b9836c5f4
Brandi Lauria <n01057930@unf.edu>
2019-12-08 22:40:42 -0500

    fixed

--- HopePipeline/Controllers/nController.cs
+++ HopePipeline/Controllers/nController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,24 +21,9 @@ namespace HopePipeline.Controllers
         }
         public IActionResult contactInfoM(string clientCode)
         {
+           
 
-            List<object> myModel = new List<object>();
-            ViewData["Message"] = "Contact Information for clients.";
-
-            var list = from r in db.client
-                       where (r.clientCode == clientCode)
-                       select r;
-            var list2 = from r in db.caregiver
-                        where (r.clientCode == clientCode)
-                        select r;
-            var list3 = from r in db.referral
-                        where (r.clientCode == clientCode)
-                        select r;
-
-            myModel.Add(list.ToList());
-            myModel.Add(list2.ToList());
-            myModel.Add(list3.ToList());
-            return View(myModel);
+            
 
         }
         public IActionResult detailReferralM()
---
+++ HopePipeline/Models/DbEntities/Referrals/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HopePipeline.Models.DbEntities.Referrals
+{
+    public class Contact
+    {
+        string Fname { get; set; }
+        string Lname { get; set; }
+        
+        string phone { get; set; }
+        string cfirst { get; set; }
+        string clast { get; set; }
+        string relationship { get; set; }
+        
+        string rName { get; set; }
+
+        string rEmail { get; set; }
+        
+    }
+}
