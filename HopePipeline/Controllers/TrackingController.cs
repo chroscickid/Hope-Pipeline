3406a54af04d86c2281d1085db20be663e378eee
chroscickid <n00963091@unf.edu>
2019-12-08 23:16:55 -0500

    MADE THE LISTS WORKgit add --all WOO-HOOgit add --all!

--- HopePipeline/Controllers/TrackingController.cs
+++ HopePipeline/Controllers/TrackingController.cs
@@ -40,20 +40,22 @@ namespace HopePipeline.Controllers
         public ViewResult TrackingList()
         {
             var results = new List<TrackingRow>();
-            string connectionString = "Server=tcp:iscrew.database.windows.net,1433;Initial Catalog=HopePipeline1;Persist Security Info=False;User ID=user;Password=pAssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string connectionString = "Data Source=iscrew.database.windows.net;Initial Catalog=HopePipeline;User ID=user;Password=pAssw0rd;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection cnn;
             cnn = new SqlConnection(connectionString);
             SqlCommand command;
             SqlDataAdapter adapter = new SqlDataAdapter();
             cnn.Open();
 
-            string query = "SELECT clientLast, clientFirst, dbo.ccr.ccrStatus, dbo.client.clientCode FROM dbo.client INNER JOIN dbo.ccr ON dbo.client.clientCode = dbo.ccr.clientCode";
+            string query = "SELECT clientLast, clientFirst, dbo.ccr.ccrStatus, dbo.client.clientCode FROM dbo.client INNER JOIN dbo.ccr ON dbo.client.clientCode = dbo.ccr.clientCode;";
             command = new SqlCommand(query, cnn);
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
                 //We push information from the query into a row and onto the list of rows
-                TrackingRow row = new TrackingRow { lname = reader.GetString(0), fname = reader.GetString(1), status = reader.GetString(2), clientCode = reader.GetString(3) };
+               // TrackingRow row = new TrackingRow { lname = reader.GetString(0), fname = reader.GetString(1), status = reader.GetString(2), clientCode = reader.GetString(3) };
+                TrackingRow row = new TrackingRow { lname = reader.GetString(0), fname = reader.GetString(1), status = reader.GetInt32(2), clientCode = reader.GetInt32(3) };
+
                 results.Add(row);
             }
             reader.Close();
---
+++ SQLQuery1.sql
@@ -0,0 +1,1 @@
+﻿SELECT clientLast, clientFirst, dbo.ccr.ccrStatus, dbo.client.clientCode FROM dbo.client INNER JOIN dbo.ccr ON dbo.client.clientCode = dbo.ccr.clientCode
