c3fb972f0376978c05420fd9ed47cddf1d40e421
chroscickid <n00963091@unf.edu>
2020-01-08 14:14:00 -0500

    NOT-WORKING! but I also added a count to the top of the tracking and referral lists

--- HopePipeline/Controllers/ReportController.cs
+++ HopePipeline/Controllers/ReportController.cs
@@ -21,69 +21,64 @@ namespace HopePipeline.Controllers
         [HttpPost]
         public IActionResult ViewReports(ReportForm genReport)
         {
-            //We call this so we can reference all the variables we need to
-            Tracking reference = new Tracking();
-            //This is a list of all the generated rows for the report
-            var results = new List<ReportRow>();
-
-            //An array of all the fields that were searched
-            string[] texts = new string[] { genReport.field1, genReport.field2, genReport.field3, genReport.field4, genReport.field5 };
-            string[] fields = new string[] { genReport.text1, genReport.text2, genReport.text3, genReport.text4, genReport.text5 };
-            
-            //Making sure that at least the first field was searched
-            //This can be fleshed out later
-            if (fields[0] == null)
-            {
-                return View("ReportError");
-            }
-
-            //Calling the SQL stuff
-            string connectionString = "placeholder";
+            var results = new List<TrackingRow>();
+            string connectionString = "Data Source=iscrew.database.windows.net;Initial Catalog=HopePipeline;User ID=user;Password=pAssw0rd;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection cnn;
             cnn = new SqlConnection(connectionString);
             SqlCommand command;
             SqlDataAdapter adapter = new SqlDataAdapter();
             cnn.Open();
 
-           
-
-            for (int i = 0; i < 5; i++)
+            string query = "SELECT clientLast, clientFirst, dbo.ccr.ccrStatus, dbo.client.clientCode FROM dbo.client INNER JOIN dbo.ccr ON dbo.client.clientCode = dbo.ccr.clientCode;";
+            command = new SqlCommand(query, cnn);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                string text = texts[i];
-                string field = fields[i];
+                for (int i = 0; i < 5; i++)
+                {
+                    string text = texts[i];
+                    string field = fields[i];
 
-                //Since yes/no/maybe fields are represented as tinyints
-                //We wanted CCR to be able to search these in plaintext, so we convert them
-                if(checkifBool(field))
-                {
-                    switch (text)
+                    //Since yes/no/maybe fields are represented as tinyints
+                    //We wanted CCR to be able to search these in plaintext, so we convert them
+                    if (checkifBool(field))
                     {
-                        case "yes":
-                            text = "0";
-                            break;
-                        case "no":
-                            text = "1";
-                            break;
-                        case "maybe":
-                            text = "2";
-                            break;
-                        default:
-                            text = null;
-                            break;
+                        switch (text)
+                        {
+                            case "yes":
+                                text = "0";
+                                break;
+                            case "no":
+                                text = "1";
+                                break;
+                            case "maybe":
+                                text = "2";
+                                break;
+                            default:
+                                text = null;
+                                break;
+                        }
                     }
-                }
-                //We generate a SQL query using the relevant field
-                string query = "SELECT [firstname],[lastname]," + field + " FROM [TrackingTable] WHERE " + field + " = " + text;
-                command = new SqlCommand(query, cnn);
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    //We push information from the query into a row and onto the list of rows
-                    ReportRow row = new ReportRow { fName = reader.GetString(0), lName = reader.GetString(1), releField1 = reader.GetString(2)};
-                    results.Add(row);
+
+                    //We generate a SQL query using the relevant field
+                    string query = "SELECT [firstname],[lastname]," + field + " FROM [TrackingTable] WHERE " + field + " = " + text;
+                    command = new SqlCommand(query, cnn);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        //We push information from the query into a row and onto the list of rows
+                        ReportRow row = new ReportRow { fName = reader.GetString(0), lName = reader.GetString(1), releField1 = reader.GetString(2) };
+                        results.Add(row);
+                    }
+                    reader.Close();
+
                 }
                 reader.Close();
 
+            return View("TrackingList", results);
+
+
+            
 
 
             }
